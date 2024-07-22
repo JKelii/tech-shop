fd44ec80296eee4c654fcea3da66f0c473027fb8
igor <igorcd98@gmail.com>
2024-07-22 15:01:10 +0200

    feat: added removeFromCart function

--- src/app/basket/page.tsx
+++ src/app/basket/page.tsx
@@ -1,47 +1,34 @@
 "use client";
-import { getProductSlug } from "@/lib";
+
 import React, { useState } from "react";
 import Image from "next/image";
-import { priceUpdate } from "@/utils/priceUpdate";
+
 import useShopContext from "@/hooks/useShopContext";
 import { X } from "lucide-react";
-import { Product } from "@/lib/hygraph/generated/graphql";
+
+import SelectQuantity from "@/components/pages/Product/components/SelectQuantity";
+import { checkCart, getCartFromCookie, removeFromCart } from "@/actions/cart";
+import { useSession } from "next-auth/react";
 
 const Page = ({ params }: { params: { slug: string } }) => {
   const slug = params.slug;
-  const { cart } = useShopContext();
+  const { cart, setCart, quantity, setQuantity } = useShopContext();
+  const session = useSession();
 
-  //TODO: fix type errors
+  const handleDelete = async () => {
+    const updatedCart = await removeFromCart({
+      product: { slug, quantity },
+    });
 
-  // const fetchProduct = async () => {
-  //   const data = await getProductSlug({ slug });
-  //   setProduct(data.product);
-  // };
+    if (updatedCart) {
+      setCart(updatedCart);
+    } else {
+      setCart([]);
+    }
+    setQuantity(0);
+  };
 
-  // fetchProduct();
-
-  // const removeFromBasket = (productToRemove: Product) => {
-  //   setBasket((currentBasket) => {
-  //     const index = currentBasket.findIndex(
-  //       (product) => product.slug === productToRemove.slug
-  //     );
-
-  //     if (index !== -1) {
-  //       return [
-  //         ...currentBasket.slice(0, index),
-  //         ...currentBasket.slice(index + 1),
-  //       ];
-  //     }
-  //     return currentBasket;
-  //   });
-  // };
-
-  // const totalPrice = basket.reduce(
-  //   (total: number, product) =>
-  //     total + Number(product.price) * Number(product.quantity),
-  //   0
-  // );
-
+  console.log(cart);
   return (
     <main className="flex flex-col justify-center items-center mt-10 mb-10 min-h-screen">
       <a
@@ -53,42 +40,41 @@ const Page = ({ params }: { params: { slug: string } }) => {
       <p className="font-bold text-3xl">
         {/* Total price: {priceUpdate(totalPrice)} */}
       </p>
-      {cart?.map((product) => (
-        <div
-          key={product.slug}
-          className="flex justify-center items-center gap-12 mt-10  p-4 border-2 border-black rounded-md"
-        >
-          <Image
-            src={product?.image}
-            alt={product?.name}
-            width={100}
-            height={100}
-            quality={100}
-            className="border-2 border-gray-500 rounded-md"
-          />
-          <div className="flex flex-col">
-            <p>
-              Quantity: <strong> {product.quantity}</strong>
-            </p>
-
-            <p>
-              Size: <strong> {product.size}</strong>
-            </p>
-          </div>
-          <div className="flex flex-col">
-            <p className="font-bold text-lg">{product?.name}</p>
-            <p className=" text-black text-xl text-center">
-              {/* {priceUpdate(product?.price * product.quantity)} */}
-            </p>
-          </div>
-          <button
-            className=" p-2 rounded-sm"
-            // onClick={() => removeFromBasket(product)}
+      {quantity >= 1 &&
+        cart?.map((product) => (
+          <div
+            key={product.slug}
+            className="flex justify-center items-center gap-12 mt-10  p-4 border-2 border-black rounded-md"
           >
-            <X />
-          </button>
-        </div>
-      ))}
+            <Image
+              src={product?.image}
+              alt={product?.name}
+              width={100}
+              height={100}
+              quality={100}
+              className="border-2 border-gray-500 rounded-md"
+            />
+
+            <div className="flex flex-col">
+              <SelectQuantity quantity={quantity} setQuantity={setQuantity} />
+
+              <div className="flx-col">
+                <p>
+                  Size: <strong> {product.size}</strong>
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col">
+              <p className="font-bold text-lg">{product?.name}</p>
+              <p className=" text-black text-xl text-center">
+                {/* {priceUpdate(product?.price * product.quantity)} */}
+              </p>
+            </div>
+            <button className=" p-2 rounded-sm" onClick={handleDelete}>
+              <X />
+            </button>
+          </div>
+        ))}
     </main>
   );
 };
